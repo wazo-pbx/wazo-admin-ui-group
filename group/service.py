da9cb21c14be541daa2d8392339b24d8bd167a82
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-03-23 21:32:40 -0400

    add and update members

--- group/service.py
+++ group/service.py
@@ -9,3 +9,31 @@ class GroupService(BaseConfdExtensionService):
 
     resource_name = 'group'
     resource_confd = 'groups'
+
+
+    def create(self, resources):
+        resource = super(GroupService, self).create(resources)
+        self._update_members(resources, resource)
+
+    def update(self, resources):
+        super(GroupService, self).update(resources)
+        self._update_members(resources)
+
+    def _update_members(self, resources, resource=None):
+        group = resources.get(self.resource_name)
+        members = group.get('users')
+
+        if resource == None:
+            resource = group['id']
+
+        if members:
+            self._update_members_to_group(resource, self._generate_users(members))
+
+    def _update_members_to_group(self, group, members):
+        return self._confd.groups.relations(group).update_user_members(members)
+
+    def _update_fallbacks_to_group(self, group, fallbacks):
+        return self._confd.groups.relations(group).update_fallbacks(fallbacks)
+
+    def _generate_users(self, users):
+        return [{'uuid': user} for user in users]
