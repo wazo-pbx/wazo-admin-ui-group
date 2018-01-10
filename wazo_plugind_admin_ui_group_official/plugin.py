b3b0d21a2909d9eb5ec901f8af4c074cf03c42e0
Cedric Abunar <cabunar@wazo.io>
2018-01-10 13:55:55 -0500

    add group funckey destination

--- wazo_plugind_admin_ui_group_official/plugin.py
+++ wazo_plugind_admin_ui_group_official/plugin.py
@@ -1,14 +1,16 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_babel import lazy_gettext as l_
 from flask_menu.classy import register_flaskview
 
 from wazo_admin_ui.helpers.plugin import create_blueprint
 from wazo_admin_ui.helpers.destination import register_destination_form, register_listing_url
+from wazo_admin_ui.helpers.funckey import register_funckey_destination_form
 
 from .service import GroupService
 from .view import GroupView, GroupDestinationView
-from .form import GroupDestinationForm
+from .form import GroupDestinationForm, GroupFuncKeyDestinationForm
 
 group = create_blueprint('group', __name__)
 
@@ -26,6 +28,7 @@ class Plugin(object):
         GroupDestinationView.register(group, route_base='/group_destination')
 
         register_destination_form('group', 'Group', GroupDestinationForm)
+        register_funckey_destination_form('group', l_('Group'), GroupFuncKeyDestinationForm)
         register_listing_url('group', 'group.GroupDestinationView:list_json')
 
         core.register_blueprint(group)
